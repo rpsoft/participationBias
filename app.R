373177ce209ec7d2a2d25ff99430bf234adcf624
Megan McMinn <megan.mcminn@glasgow.ac.uk>
2018-11-14 11:23:08 +0000

    adding context

--- app.R
+++ app.R
@@ -15,34 +15,79 @@ source("misclassification_bias_2019.R")
 
 # Define UI for application that draws a histogram
 ui <- fluidPage(
-   includeCSS("styles.css"),
+   #includeCSS("styles.css"),
   
-   # Application title
-   titlePanel("Non-participation bias"),
-          h4("Using only data from the Participants of the study, finding borderline significant value, and a <1 OR, suggesting that regular use is assoc with lower odds of developing melanoma",class="header"),
-          dataTableOutput('table1'),
-          textOutput("fisher1"),
-          hr(),
-          h4("Using only the data from the Non-participants, we find an OR > 1, and is not significant.",class="header"),
-          dataTableOutput('table2'),
-          textOutput("fisher2"),
-          hr(),
-          h4("Combining both participants and non-participants, we see the 'true' OR for this sample is 1.25.",class="header"),
-          dataTableOutput('table3'),
-          textOutput("fisher3")
+   
+   titlePanel("Non-participation bias example"),
+   h3("Mobile Phone Use and Risk of Uveal Melanoma: Results of the Risk Factors for Uveal Melanoma Case-Control Study"),
+   h6("Stang, A. et al. (2009) Journal of the National Cancer Institute, https://doi.org/10.1093/jnci/djn441
+    and 
+    Lash, T.L, Fox, M.P, and Fink, A.K. (2009) Applying Quantitative Bias Analysis to Epidemiologic Data)"), 
+
+  p("The study by Stang et al. (2009) reported on a case-control study investigating the association between mobile phone use and risk of uveal melanoma. 
+      Cases for the study were recruited from a referral centre, and three types of controls (population, siblings and opthalmologists).", 
+       br(),
+       br(),
+      "We're going to focus on the analysis using the population controls. 
+
+      486 Cases were identified as being eligable for the study, and 459 went on to participate (94%). 
+      Matched controls were selected from the population, and 55% agreed to participate. 
+      The questionnaire asked participants to rate mobile phone use as 'Never', 'Sporodic' and 'Regular' (amongst other questions)",
+    br(),
+    br()),
+   
+   navlistPanel(
+     "Status",
+     tabPanel("Participants",
+              p("Using only data from the participants of the study, we compare those who report regular use and those who report never using a mobile phone"),
+              DT::dataTableOutput('table1'),
+              textOutput("fisher1"),
+              p("The Odds Ratio is < 1. This suggests that regular use of a mobile phone", em("decreases"), "the odds of developing of uveal melanoma. 
+                The 95% CI does not include the null value of 1, incating evidence of a significant association",
+                br(),
+                br(),
+                "Now, consider the differences in rates of participation between the cases (94%) and the controls (55%). This suggests there may be a non-participation bias")
+     ),
+     tabPanel("Non-participants",
+              p("Those who declined to participate in the larger study were recontacted and asked if they would answer a shorter question.", 
+                br(),
+                 "10 of the 27 non-participating cases and 284 of the 663 non-participating controls completed this questionnaire. 
+                 Only two categories were included: Regular use or no use"),
+              DT::dataTableOutput('table2'),
+              textOutput("fisher2"),
+              p(br(), "Using the data from the non-participants, the odds ratio is now > 1, suggetsing that odds of uveal melanoma are", em("increased"), "with regular Use of a mobile phone
+                The 95% confidence interval includes the null value of 1, therefore, the increase is not statistically significant.",
+                br(),
+                br(),
+                "Let's look at what the results are if we combine the data from participants and non-participants.")
+              
+     ),
+     tabPanel("Participants & Non-participants",
+              p("We can combine the data for participants and non-participants:"),
+              DT::dataTableOutput('table3'),
+              textOutput("fisher3"),
+              br(),
+              p("The 95% confidence interval includes the null value of 1, therefore, in the full* sample, there is no increased odds in being diagnosed with uveal melanoma if you're a regular mobile phone user, compared to a non-user.",
+                br(),
+                br(),
+                "* Note that this isn't really the full sample - only some of the non-participants agreed to complete the smaller questionnaire")
+              
+     )
+   )
 )
+
 
 # Define server logic required to draw a histogram
 server <- function(input, output) {
 
-    output$table1 <- renderDataTable(stang09_p, options = list(paging = FALSE,searching = FALSE) )
-    output$fisher1 <- renderText( paste0("Fisher test values : ", f1r) )
+    output$table1 <- DT::renderDataTable({ DT::datatable(stang09_p, rownames=TRUE,  filter='none', options = list(dom = 't'))})
+    output$fisher1 <- renderText( paste0("Odds Ratio = ", f1r, "   95%CI = (", f1ll, ", ", f1ul, ")" ))
   
-    output$table2 <- renderDataTable(stang09_np_short, options = list(paging = FALSE,searching = FALSE) )
-    output$fisher2 <- renderText( paste0("Fisher test values : ", f2r) )
+    output$table2 <- DT::renderDataTable({ DT::datatable(stang09_np_short, rownames=TRUE,  filter='none', options = list(dom = 't'))})
+    output$fisher2 <- renderText( paste0("Odds Ratio = ", f2r, "   95%CI = (", f2ll, ", ", f2ul, ")" ))
     
-    output$table3 <- renderDataTable(stang09_full, options = list(paging = FALSE,searching = FALSE) )
-    output$fisher3 <- renderText( paste0("Fisher test values : ", ffr) )
+    output$table3 <- DT::renderDataTable({ DT::datatable(stang09_full, rownames=TRUE,  filter='none', options = list(dom = 't'))})
+    output$fisher3 <- renderText( paste0("Odds Ratio = ", ffr, "   95%CI = (", ffll, ", ", fful, ")" ))
     
 }
 # Run the application 
--- misclassification_bias_2019.R
+++ misclassification_bias_2019.R
@@ -6,81 +6,6 @@
 # R 3.5.1
 #################################################
 
-
-#################################################
-# Non-differential misclassifcation of exposure #
-#################################################
-# Hypothetical data and situation is taken from Rothman 2012 (Epidemiology: an introduction 2nd ed. p. 135)
-
-## Background text to include as explanation:
-# This table (true.dist) reprents hypothetical data for a Case-Control study investigating the effects of a high fat diet on the odds of having a subsequent Myocardial infarction. 
-# The participant's diets are recorded, and classified as 'high fat' or 'not'. As there will be an arbitrary cut-off point at which a diet is regarded as being 'high fat',
-# it is plausible that some of the participants will have their exposure status misclassified. 
-
-## This first table shows the situation where all subjects are correctly classified with respect 
-## to the outcome (i.e. MI), and the exposure (i.e. eating a high fat diet).
-## In reality, this distribtion is almost never truely know. 
-
-true.dist <- as.data.frame(matrix(c(250, 450, 100, 900), byrow = FALSE, nrow = 2, ncol = 2), row.names = c("Exposed", "Unexposed"))
-colnames(true.dist) <- c("MI Case", "Control")
-round(fisher.test(true.dist)$estimate,2)
-
-## Now we investigate the effects of non-differential misclassification of their exposure status. 
-## (i.e. some of the exposed subjects are incorrectly classified as 'not exposed' and vice versa.)
-
-## Misclassifies exposure status (both ways). Disease status doesn't change. 
-miscl.exposure <- function(true.data, exp.perc, unexp.perc) {
-  ## First, calculate the table for the misclassified unexposed
-  new.case.exp <- round(true.data[1,1] + (true.data[2,1]*unexp.perc),0)
-  new.case.unexp <- sum(true.data[,1]) - new.case.exp
-  new.table <- c(new.case.exp, new.case.unexp) ## column 1 cases
-  new.cont.exp <- round(true.data[1,2] + (true.data[2,2]*unexp.perc),0)
-  new.cont.unexp <- sum(true.data[,2]) - new.cont.exp
-  new.table <- cbind(new.table, c(new.cont.exp, new.cont.unexp)) ## column 2 controls
-
-  # Then do the misclassified exposed
-  new.case.unexp <- round(new.table[2,1] + (true.data[1,1]*exp.perc),0)
-  new.case.exp <- sum(new.table[,1]) - new.case.unexp
-  new.table2 <- c(new.case.exp, new.case.unexp) ## column 1 cases
-  new.cont.unexp <- round(new.table[2,2] + (true.data[1,2]*exp.perc),0)
-  new.cont.exp <- sum(new.table[,2]) - new.cont.unexp
-  new.table2 <- cbind(new.table2, c(new.cont.exp, new.cont.unexp)) ## column 2 controls
-  
-  colnames(new.table2) <- c("MI Case", "Control")
-  rownames(new.table2) <- c("Exposed", "Unexposed")
-  ## Results
-
-  a = new.table2
-  b = round(fisher.test(new.table2)$estimate,1)
-  c = c(round(fisher.test(new.table2, conf.int = TRUE)$conf.int[1],1), round(fisher.test(new.table2, conf.int = TRUE)$conf.int[2],1))
-  
-  return(list(a,b,c))
- 
- }
-
-# This is the correct classification, giving us the true OR = 5.0
-miscl.exposure(true.dist, 0, 0) -> D
-
-D[[1]] %>% typeof()
-
-
-
-
-# Now we consider the case where 20% of the 'unexposed' to having a high fat diet:
-miscl.exposure(true.dist, exp.perc = 0, unexp.perc = 0.2)
-# Finally, where 20% of each exposure category are misclassified:
-miscl.exposure(true.dist, exp.perc = 0.2, unexp.perc = 0.2)
-miscl.exposure(true.dist, exp.perc = 0.5, unexp.perc = 0.7)
-
-## In each case, we see that the non-differential misclassification of the exposure biases the result towards the null. 
-## Nondifferential misclassification of a dichotomous exposure always biases toward the null. 
-
-## Shiny app:
-## I'm envisioning two sliders, allowing the students to change the amounts of exp.perc and unexp.perc (each ranging from 0 - 100%) 
-## The table can be updated from the sliders, to show the new numbers of exp/unexposed in each group, and the OR results below. 
-## (or keep a copy of the true.dist table, and the new table appears next to it to allow for comparison?)
-
-
 ###########################
 # Non-participation bias  #
 ###########################
@@ -107,19 +32,25 @@ colnames(stang09_np_short) <- c("Case", "Control")
 # suggesting that regular use is assoc with lower odds of developing melanoma
 fisher.test(stang09_p) -> f1
 round(fisher.test(stang09_p)$estimate,2) -> f1r
+f1ll <- round(fisher.test(stang09_p, conf.int = TRUE)$conf.int[1],2)
+f1ul <- round(fisher.test(stang09_p, conf.int = TRUE)$conf.int[2],2) 
 # 0.71
 
 # Using only the data from the Non-participants, we find an OR > 1, and is not significant. 
-fisher.test(stang09_np_short) -> f2
-round(fisher.test(stang09_np_short)$estimate,2) ->f2r
+f2 <- fisher.test(stang09_np_short)
+f2r <- round(fisher.test(stang09_np_short)$estimate,2)
+f2ll <- round(fisher.test(stang09_np_short, conf.int = TRUE)$conf.int[1],2)
+f2ul <- round(fisher.test(stang09_np_short, conf.int = TRUE)$conf.int[2],2) 
 # 1.26
 
 ## This shows the impact of non-participation bias
 
 ## Combining both participants and non-participants, we see the 'true' OR for this sample is 1.25. 
 stang09_full <- stang09_p + stang09_np_short
-fisher.test(stang09_full) -> ff
-round(fisher.test(stang09_full)$estimate,2) ->ffr
+ff <- fisher.test(stang09_full)  
+ffr <- round(fisher.test(stang09_full)$estimate,2)
+ffll <- round(fisher.test(stang09_full, conf.int = TRUE)$conf.int[1],2)
+fful <- round(fisher.test(stang09_full, conf.int = TRUE)$conf.int[2],2) 
 # 1.25
 
 ## Discussion can be had on non-participants who refused to take part in the short questionnaire 
